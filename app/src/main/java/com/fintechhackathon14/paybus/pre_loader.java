a4d275fedbc0c6252f703ef4cd9c90c2edcc1a27
super <superterrafox@gmail.com>
2022-06-26 04:30:20 +0600

    ошибку в тексте исправил

--- app/src/main/java/com/fintechhackathon14/paybus/pre_loader.java
+++ app/src/main/java/com/fintechhackathon14/paybus/pre_loader.java
@@ -17,7 +17,7 @@ public class pre_loader extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(pre_loader.this, auth_activity.class));
+                startActivity(new Intent(pre_loader.this, MainActivity.class));
                 finish();
             }
         }, 3000);
